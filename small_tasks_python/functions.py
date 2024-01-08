f836ed4659e5905a43701c61f24e744f9e2e29ae
TaisaKiarkhi <tayisiyakiarkhi@gmail.com>
2024-01-08 19:45:19 +0100

    upd

--- small_tasks_python/functions.py
+++ small_tasks_python/functions.py
@@ -1,3 +1,7 @@
+
+ID_dict = {}
+Information = []
+
 def calc_age(age):
     days = age * 365
     return days
@@ -9,3 +13,13 @@ power = lambda x,y: x ** y
 def products_dict(meat_, fish_, **new_category):
         merged = {**meat_, **fish_, **new_category}
         return merged
+
+def info(position, company, *personal_info, country, city, ID):
+    personal_information = (personal_info)
+    work_place = (position, company)
+    location = (country, city)
+    global Information
+    Information.append(personal_information, work_place, location)
+    
+    global ID_dict
+    ID_dict = {ID: Information[-1]}
